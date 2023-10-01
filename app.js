9a018b180753ed1f17cfa420f308f5967a8c2663
jhanikhilnath <jhanikhil19122007@gmail.com>
2023-10-01 19:12:20 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var main = document.querySelector('.bye');
+var iphone = document.querySelector('.hello');
+
+window.addEventListener('scroll', function () {
+	if (pageYOffset > 200) {
+		x.classList.add('animate__animated', 'animate__lightSpeedInRight');
+	}
+});
